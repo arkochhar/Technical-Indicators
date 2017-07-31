e7f342a2f7649ca4b7b0a1544d0b3f138f69f8dd
Anuraag <arkochhar@hotmail.com>
2017-07-31 19:06:36 +0530

    Branch for first release and pull request

--- indicator/__init__.py
+++ indicator/__init__.py
@@ -5,7 +5,7 @@ Package: indicator
 Author: Anuraag Rai Kochhar
 Email: arkochhar@hotmail.com
 Repository: https://github.com/arkochhar/Technical-Indicators
-Version: 0.0.1
+Version: 1.0.0
 License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007
 """
 
